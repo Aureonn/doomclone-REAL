77032f5052ef6168dfbfbb75e38f0b8e0cb5015e
Aureonn <88062425+Aureonn@users.noreply.github.com>
2022-05-31 12:24:01 +1200

    mouse look done
    fix the bug of the 2 maps
    1:47 of video 5

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -25,7 +25,10 @@ func get_input():
 	return input_dir
 	
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp ($Pivot.rotation.x, -1.2, 1.2)
 	
 func _physics_process(delta):
 	velocity.y += gravity * delta
